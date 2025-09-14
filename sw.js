bbbfea1542d2302debb17be1d69f6beffb0c2087
imigoser <imigoser@gmail.com>
2025-09-14 13:57:22 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('v1').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon.png',
+      './character.png'
+    ]))
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
